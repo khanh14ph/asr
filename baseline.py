7c6d749a7cd525680f87d7f133faf15125db5b67
khanh14ph <72318494+khanh14ph@users.noreply.github.com>
2024-07-04 17:36:57 +0700

    Update baseline.py

--- baseline.py
+++ baseline.py
@@ -1,80 +1,95 @@
-from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
-import librosa
-import torch
-import numpy as np
-import tqdm
-import librosa
+from typing import Dict, List, Optional, Union
+import warnings
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-from transformers import AutoModel, AutoTokenizer
+warnings.filterwarnings("ignore")
+
 import numpy as np
-import json
 from datasets import load_dataset, load_metric
-wer_metric = load_metric("wer")
-
-import pandas as pd
-from tqdm import tqdm
-import numpy as np
-import random
-import json
-import re
-
-import datasets
-from datasets import ClassLabel
-from datasets import load_dataset, load_metric
-
-from dataclasses import dataclass, field
-from typing import Any, Dict, List, Optional, Union
-
+from dataclasses import dataclass
 from transformers import Trainer
+
+from transformers import Wav2Vec2ForCTC
+from model import Wav2Vec2ForCTCKan
+# from model import Wav2Vec2ForCTC
 from transformers import TrainingArguments
 from transformers import Wav2Vec2Processor
 from transformers import Wav2Vec2FeatureExtractor
 from transformers import Wav2Vec2CTCTokenizer
-
 import torch
 import torchaudio
+import os
+# import config_pseudo as config
+# import config_general as config
+# import config_kan as config
+# import config_topline as config
 
-import warnings
-warnings.filterwarnings("ignore")
-root="/kaggle/input/vlsp-asr/"
-train_dataset = load_dataset("csv", data_files='/kaggle/input/vlsp-asr/VLSP/train/train_simple.csv.txt', split="train")
-test_dataset = load_dataset("csv", data_files='/kaggle/input/vlsp-asr/VLSP/dev/dev_simple.csv.txt', split="train")
+import config
+if os.path.exists("cache")==False:
+    os.mkdir("cache")
+print(config.cache_dir)
+train_dataset = load_dataset(
+    "csv", data_files=config.training_data, split="train", cache_dir=config.cache_dir, sep="|"
+)
+# random_indices = [i for i in range(1000)]
+# train_dataset = train_dataset.select(random_indices)
+# train_dataset = train_dataset.filter(lambda example: example["is_groundtruth"] == True)
+print("LEN: ", len(train_dataset))
+chars_to_ignore_regex = (
+    '[\,\̀\#\̃\_\̣\=\$\&\̉\?\̀\(\)\+\/\_\'"\{\}\<\>\|\`\~\*\&\^\@\.\!\́\-\;\:"\“\%\‘\”\�]'
+)
 
-tokenizer = Wav2Vec2CTCTokenizer(root+"VLSP/vocab.json.txt", unk_token="<unk>", pad_token="<pad>", word_delimiter_token="|")
-feature_extractor = Wav2Vec2FeatureExtractor(feature_size=1, sampling_rate=16000, padding_value=0.0, do_normalize=True, return_attention_mask=True)
+tokenizer = Wav2Vec2CTCTokenizer(
+    config.vocab_file,
+    unk_token="[UNK]",
+    pad_token="[PAD]",
+    word_delimiter_token="|",
+    bos_token=None,
+    eos_token=None,
+)
+feature_extractor = Wav2Vec2FeatureExtractor(
+    feature_size=1,
+    sampling_rate=16000,
+    padding_value=0.0,
+    do_normalize=True,
+    return_attention_mask=True,
+)
 processor = Wav2Vec2Processor(feature_extractor=feature_extractor, tokenizer=tokenizer)
 
-# processor = Wav2Vec2Processor.from_pretrained("/home2/khanhnd/contextualized_asr/output_dir/checkpoint-500",unk_token="[unk]", pad_token="[pad]", word_delimiter_token="|")
-print(processor.tokenizer.pad_token_id)
+
 def speech_file_to_array_fn(batch):
-    speech_array, sampling_rate = torchaudio.load(root+batch["audio"])
+    speech_array, sampling_rate = torchaudio.load(batch["audio_path"])
+    new_sr = 16000
+    speech_array = torchaudio.functional.resample(
+        speech_array, orig_freq=sampling_rate, new_freq=new_sr
+    )
     batch["speech"] = speech_array[0].numpy()
-    batch["sampling_rate"] = sampling_rate
-    batch["target_text"] = batch["text"]
-    return batch
+    batch["sampling_rate"] = new_sr
+    batch["target_text"] = (
+        batch["transcription"] if batch["transcription"] is not None else "[UNK]"
+    )
+    batch["input_values"] = processor(
+        [batch["speech"]], sampling_rate=batch["sampling_rate"]).input_values[0]
 
-train_dataset = train_dataset.map(speech_file_to_array_fn, remove_columns=train_dataset.column_names)
-test_dataset = test_dataset.map(speech_file_to_array_fn, remove_columns=test_dataset.column_names)
-
-def prepare_dataset(batch):
-    # check that all files have the correct sampling rate
-    assert (
-        len(set(batch["sampling_rate"])) == 1
-    ), f"Make sure all inputs have the same sampling rate of {processor.feature_extractor.sampling_rate}."
-
-    batch["input_values"] = processor(batch["speech"], sampling_rate=batch["sampling_rate"][0]).input_values
-    
     with processor.as_target_processor():
-        batch["labels"] = processor(batch["target_text"]).input_ids
+        batch["labels"] = processor([batch["target_text"]]).input_ids[0]
+    batch.pop("sampling_rate")
+    batch.pop("speech")
+    batch.pop("audio_path")
+    
+    # "speech","audio_path")
     return batch
 
-train_dataset = train_dataset.map(prepare_dataset, remove_columns=train_dataset.column_names, batch_size=8, num_proc=8, batched=True)
-test_dataset = test_dataset.map(prepare_dataset, remove_columns=test_dataset.column_names, batch_size=8, num_proc=8, batched=True)
+
+func_suffix = ""
+
+train_dataset = train_dataset.map(
+    speech_file_to_array_fn,
+     num_proc=8,
+)
+print(train_dataset)
 
 @dataclass
 class DataCollatorCTCWithPadding:
-
     processor: Wav2Vec2Processor
     padding: Union[bool, str] = True
     max_length: Optional[int] = None
@@ -82,15 +97,19 @@ class DataCollatorCTCWithPadding:
     pad_to_multiple_of: Optional[int] = None
     pad_to_multiple_of_labels: Optional[int] = None
 
-    def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
-        # split inputs and labels since they have to be of different lengths and need
+    def __call__(
+        self, features: List[Dict[str, Union[List[int], torch.Tensor]]]
+    ) -> Dict[str, torch.Tensor]:
+        # split inputs and labels since they have to be of different lenghts and need
         # different padding methods
-        input_features = [{"input_values": feature["input_values"]} for feature in features]
+        input_features = [
+            {"input_values": feature["input_values"]} for feature in features
+        ]
         label_features = [{"input_ids": feature["labels"]} for feature in features]
 
         batch = self.processor.pad(
             input_features,
-            padding=self.padding,
+            padding=True,
             max_length=self.max_length,
             pad_to_multiple_of=self.pad_to_multiple_of,
             return_tensors="pt",
@@ -98,24 +117,46 @@ class DataCollatorCTCWithPadding:
         with self.processor.as_target_processor():
             labels_batch = self.processor.pad(
                 label_features,
-                padding=self.padding,
+                padding=True,
                 max_length=self.max_length_labels,
                 pad_to_multiple_of=self.pad_to_multiple_of_labels,
                 return_tensors="pt",
             )
 
         # replace padding with -100 to ignore loss correctly
-        labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
+        labels = labels_batch["input_ids"].masked_fill(
+            labels_batch.attention_mask.ne(1), -100
+        )
 
         batch["labels"] = labels
-
+        # print(batch.keys())
         return batch
+
 
 data_collator = DataCollatorCTCWithPadding(processor=processor, padding=True)
 
-print("LEN VOCAB", len(processor.tokenizer))
+wer_metric = load_metric("wer")
+
+
+def compute_metrics(pred):
+    pred_logits = pred.predictions
+    pred_ids = np.argmax(pred_logits, axis=-1)
+    pred.label_ids[pred.label_ids == -100] = processor.tokenizer.pad_token_id
+
+    pred_str = processor.batch_decode(pred_ids)
+    # we do not want to group tokens when computing the metrics
+    label_str = processor.batch_decode(pred.label_ids, group_tokens=False)
+
+    label_str = ["im lặng" if label == "" else label for label in label_str]
+    pred_str = ["im lặng" if pred == "" else pred for pred in pred_str]
+
+    wer = wer_metric.compute(predictions=pred_str, references=label_str)
+
+    return {"wer": wer}
+
+
 model = Wav2Vec2ForCTC.from_pretrained(
-    "facebook/wav2vec2-base", 
+    config.pretrained_model,
     attention_dropout=0.1,
     hidden_dropout=0.1,
     feat_proj_dropout=0.1,
@@ -125,51 +166,33 @@ model = Wav2Vec2ForCTC.from_pretrained(
     ctc_loss_reduction="mean",
     ctc_zero_infinity=True,
     pad_token_id=processor.tokenizer.pad_token_id,
-    vocab_size=len(processor.tokenizer)-2,
-    num_labels=2,
-).to(device)
-model.freeze_feature_extractor()
-
-from transformers import TrainingArguments
-
-training_args = TrainingArguments(
-  output_dir="output_dir",
-  group_by_length=True,
-  per_device_train_batch_size=4,
-  evaluation_strategy="steps",
-  num_train_epochs=100,
-  fp16=True,
-  gradient_checkpointing=True, 
-  save_steps=2000,
-  eval_steps=2000,
-  logging_steps=500,
-  dataloader_num_workers=6,
-  learning_rate=5e-6,
-  weight_decay=0.005,
-  warmup_steps=1000,
-  save_total_limit=2,
-  eval_accumulation_steps=1,
-  load_best_model_at_end=True,
-  greater_is_better=False,
-  metric_for_best_model="wer",
-  report_to='tensorboard'
+    vocab_size=len(processor.tokenizer),
 )
 
+model.freeze_feature_extractor()
 
-def compute_metrics(pred):
-    pred_logits = pred.predictions
-    pred_ids = np.argmax(pred_logits, axis=-1)
-
-    pred.label_ids[pred.label_ids == -100] = processor.tokenizer.pad_token_id
-
-    pred_str = processor.batch_decode(pred_ids)
-    # we do not want to group tokens when computing the metrics
-    label_str = processor.batch_decode(pred.label_ids, group_tokens=False)
-
-    wer = wer_metric.compute(predictions=pred_str, references=label_str)
-
-    return {"wer": wer}
-
+training_args = TrainingArguments(
+    output_dir=config.checkpoint_dir,
+    group_by_length=False,
+    per_device_train_batch_size=config.batch_size,
+    gradient_accumulation_steps=2,
+    save_strategy="steps",
+    logging_strategy="steps",
+    num_train_epochs=config.num_epochs,
+    ignore_data_skip=False,
+      save_steps=2000,
+    #   eval_steps=200000000000000000,
+    logging_steps=100,
+    #   evaluation_strategy="epoch",
+    dataloader_num_workers=6,
+    learning_rate=config.learning_rate,
+    warmup_steps=0,
+    #   label_smoothing_factor=0.1,
+      save_total_limit=5,
+    #   eval_accumulation_steps=1,
+    report_to="tensorboard",
+    #   load_best_model_at_end=True
+)
 
 trainer = Trainer(
     model=model,
@@ -177,9 +200,8 @@ trainer = Trainer(
     args=training_args,
     compute_metrics=compute_metrics,
     train_dataset=train_dataset,
-    eval_dataset=test_dataset,
     tokenizer=processor.feature_extractor,
+    # callbacks = [EarlyStoppingCallback(early_stopping_patience=3)]
 )
-print("START")
 trainer.train()
 # trainer.train(resume_from_checkpoint=True)
